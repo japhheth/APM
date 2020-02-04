2ce1121b605722ccc36bf1cb0335f5401dd6c6b6
japhheth <japhheth@gmail.com>
2020-02-04 08:21:31 +0100

    'commit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component } from '@angular/core';
 
 @Component({
+
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:  `<div> 
+                <h1>{{ pageTitle }}</h1>
+                <div>My first Component</div>
+  
+                 </div>`
+
 })
-export class AppComponent {
-  title = 'APM';
+
+
+
+
+
+
+export class AppComponent{
+  pageTitle : string = 'Acme Product Management'
 }
